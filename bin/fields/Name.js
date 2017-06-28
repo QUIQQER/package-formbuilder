1010ee537ce035d5816206ec8dc075cc5008252e
Henning Leutz <leutz@pcsg.de>
2017-06-28 17:52:50 +0200

    fix: quiqqer/package-formbuilder#7 - Name Field -> Übersetzbar machen

--- bin/fields/Name.js
+++ bin/fields/Name.js
@@ -5,17 +5,20 @@
  * @author www.pcsg.de (Henning Leutz)
  *
  * @reuquire package/quiqqer/formbuilder/bin/FormField
+ * @reuquire Mustache
  * @reuquire text!package/quiqqer/formbuilder/bin/fields/Name.html
  * @reuquire css!package/quiqqer/formbuilder/bin/fields/Name.css
  */
 define('package/quiqqer/formbuilder/bin/fields/Name', [
 
     'package/quiqqer/formbuilder/bin/FormField',
-    'text!package/quiqqer/formbuilder/bin/fields/Name.html',
+    'Mustache',
+    'Locale',
 
+    'text!package/quiqqer/formbuilder/bin/fields/Name.html',
     'css!package/quiqqer/formbuilder/bin/fields/Name.css'
 
-], function (Field, body) {
+], function (Field, Mustache, QUILocale, body) {
     "use strict";
 
     return new Class({
@@ -25,11 +28,17 @@ define('package/quiqqer/formbuilder/bin/fields/Name', [
 
         Binds: [
             '$onCreate',
-            '$onGetSettings'
+            '$onGetSettings',
+            '$inputChange',
+            '$refreshData'
         ],
 
         options: {
-            extend: false
+            extend: false,
+            title : QUILocale.get('quiqqer/quiqqer', 'title'),
+            first : QUILocale.get('quiqqer/quiqqer', 'first'),
+            last  : QUILocale.get('quiqqer/quiqqer', 'last'),
+            suffix: QUILocale.get('quiqqer/quiqqer', 'suffix')
         },
 
         initialize: function (options) {
@@ -45,7 +54,12 @@ define('package/quiqqer/formbuilder/bin/fields/Name', [
          * event : on create
          */
         $onCreate: function () {
-            this.getBody().set('html', body);
+            this.getBody().set('html', Mustache.render(body, {
+                title : this.getAttribute('title'),
+                first : this.getAttribute('first'),
+                last  : this.getAttribute('last'),
+                suffix: this.getAttribute('suffix')
+            }));
 
             if (this.getAttribute('extend')) {
                 this.getBody().addClass('form-name--extend');
@@ -60,9 +74,11 @@ define('package/quiqqer/formbuilder/bin/fields/Name', [
          * @param {HTMLElement} Elm
          */
         $onGetSettings: function (self, Elm) {
+            this.$SettingsContainer = new Element('div', {
+                'class': 'qui-formfield-settings-setting'
+            }).inject(Elm);
 
-            this.$SettingsContainer = new Element('div').inject(Elm);
-
+            // #locale
             new Element('label', {
                 html: '<input type="checkbox" name="extend" />' +
                       '<span>Erweitert</span>'
@@ -71,7 +87,7 @@ define('package/quiqqer/formbuilder/bin/fields/Name', [
             var Extend = this.$SettingsContainer.getElement('[name="extend"]');
 
             Extend.addEvents({
-                change : function() {
+                change: function () {
                     self.setAttribute('extend', this.checked);
 
                     if (this.checked) {
@@ -85,6 +101,68 @@ define('package/quiqqer/formbuilder/bin/fields/Name', [
             if (this.getAttribute('extend')) {
                 Extend.set('checked', true);
             }
+
+
+            var Title = new Element('div', {
+                'class': 'qui-formfield-settings-setting',
+                html   : '<label>' +
+                         '</label><span class="qui-formfield-settings-setting-title">Title</span>' +
+                         '<input name="title" type="text" />' +
+                         '</label>'
+            }).inject(Elm);
+
+            var First = new Element('label', {
+                'class': 'qui-formfield-settings-setting',
+                html   : '<label>' +
+                         '<span class="qui-formfield-settings-setting-title">First</span>' +
+                         '<input name="first" type="text" />' +
+                         '</label>'
+            }).inject(Elm);
+
+            var Last = new Element('label', {
+                'class': 'qui-formfield-settings-setting',
+                html   : '<label>' +
+                         '<span class="qui-formfield-settings-setting-title">Last</span>' +
+                         '<input name="last" type="text" />' +
+                         '</label>'
+            }).inject(Elm);
+
+            var Suffix = new Element('label', {
+                'class': 'qui-formfield-settings-setting',
+                html   : '<label>' +
+                         '<span class="qui-formfield-settings-setting-title">suffix</span>' +
+                         '<input name="suffix" type="text" />' +
+                         '</label>'
+            }).inject(Elm);
+
+
+            Title.getElement('[name="title"]').addEvent('change', this.$inputChange);
+            First.getElement('[name="first"]').addEvent('change', this.$inputChange);
+            Last.getElement('[name="last"]').addEvent('change', this.$inputChange);
+            Suffix.getElement('[name="suffix"]').addEvent('change', this.$inputChange);
+        },
+
+        /**
+         * input change event
+         * @param event
+         */
+        $inputChange: function (event) {
+            var Input = event.target;
+
+            this.setAttribute(Input.name, Input.value);
+            this.$refreshData();
+        },
+
+        /**
+         * refresh data helper
+         */
+        $refreshData: function () {
+            this.getBody().set('html', Mustache.render(body, {
+                title : this.getAttribute('title'),
+                first : this.getAttribute('first'),
+                last  : this.getAttribute('last'),
+                suffix: this.getAttribute('suffix')
+            }));
         }
     });
 });
--- src/QUI/FormBuilder/Fields/Name.php
+++ src/QUI/FormBuilder/Fields/Name.php
@@ -3,6 +3,7 @@
 /**
  * This file contains \QUI\FormBuilder\Fields\Name
  */
+
 namespace QUI\FormBuilder\Fields;
 
 use QUI\FormBuilder;
@@ -22,6 +23,11 @@ class Name extends FormBuilder\Field
     {
         $file    = OPT_DIR . 'quiqqer/formbuilder/bin/fields/Name.html';
         $content = file_get_contents($file);
+
+        $content = str_replace('{{title}}', $this->getAttribute('title'), $content);
+        $content = str_replace('{{first}}', $this->getAttribute('first'), $content);
+        $content = str_replace('{{last}}', $this->getAttribute('last'), $content);
+        $content = str_replace('{{suffix}}', $this->getAttribute('suffix'), $content);
 
         if ($this->getAttribute('extend')) {
             $content = '<div class="form-name--extend">' . $content . '</div>';
